35554c1d1f54061b1df614fe147cc062d498fc30
Zachary Piontek <fortunefavorsthebrave@protonmail.com>
2022-07-28 10:10:57 -0700

    inserted data postresql with setup

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,41 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP table if exists nfl;
+
+CREATE table nfl (
+    id BIGINT NOT NULL,
+    name VARCHAR NOT NULL,
+    city VARCHAR NOT NULL,
+    state VARCHAR NOT NULL,
+    url VARCHAR NOT NULL
+);
+
+INSERT into nfl (id, name, city, state, url) values
+    (
+        '1',
+        'Cardinals',
+        'Phoenix',
+        'Arizona',
+        'https://en.wikipedia.org/wiki/Arizona_Cardinals'
+    ),
+    (
+        '2',
+        'Falcons',
+        'Atlanta',
+        'Georgia',
+        'https://en.wikipedia.org/wiki/Atlanta_Falcons'
+    ),
+    (
+        '3',
+        'Ravens',
+        'Baltimore',
+        'Maryland',
+        'https://en.wikipedia.org/wiki/Baltimore_Ravens'
+    ),
+    (
+        '4',
+        'Bills',
+        'Buffalo',
+        'New York',
+        'https://en.wikipedia.org/wiki/Buffalo_Bills'
+    );
